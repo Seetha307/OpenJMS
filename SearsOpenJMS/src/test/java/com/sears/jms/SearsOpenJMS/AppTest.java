c27f4ae17bc05166afa7404daeb8bc41f58fe1e3
Seetharaman_M <Seetharaman_M@CHNMCT118749L.ad.infosys.com>
2016-09-03 15:53:10 -0500

    Adding changes for OpenJMS

---
+++ SearsOpenJMS/src/test/java/com/sears/jms/SearsOpenJMS/AppTest.java
@@ -0,0 +1,42 @@
+package com.sears.jms.SearsOpenJMS;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	
+    	EventMessage eventMessage = new EventMessage(1,	"Message from jUnitTest");
+    	SendClient firstClient = new SendClient();
+		firstClient.sendMessage();
+    	assertTrue( true );
+    }
+}
